0619416e1570062fee6cd94569064aab3ec25ac8
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-06-13 04:56:51 +0000

    test 1

--- src/components/Step.js
+++ src/components/Step.js
@@ -23,30 +23,30 @@ const Step = ({ step, onNext, onPrev, onSubmit }) => {
     switch (step) {
       case 1:
         return (
-          <>
+          <div id='stem1'>
             <label htmlFor="first_name">First Name:</label>
             <input type="text" id="first_name" name="firstName" onChange={handleChange} />
             <label htmlFor="last_name">Last Name:</label>
             <input type="text" id="last_name" name="lastName" onChange={handleChange} />
-          </>
+          </div>
         );
       case 2:
         return (
-          <>
+          <div id='step2'>
             <label htmlFor="model">Car Model:</label>
             <input type="text" id="model" name="model" onChange={handleChange} />
             <label htmlFor="car_price">Car Price:</label>
             <input type="number" id="car_price" name="car_price" onChange={handleChange} />
-          </>
+          </div>
         );
       case 3:
         return (
-          <>
+          <div id='step3'>
             <label htmlFor="card_info">Card Information:</label>
             <input type="text" id="card_info" name="card_info" onChange={handleChange} />
             <label htmlFor="expiry_date">Expiry Date:</label>
             <input type="date" id="expiry_date" name="expiry_date" onChange={handleChange} />
-          </>
+          </div>
         );
       default:
         return null;
